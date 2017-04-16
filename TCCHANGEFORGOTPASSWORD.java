3606a9008428dc49b68565be2a99d77580848bd3
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2017-04-15 20:50:30 -0500

    Test Scripts for County_health_system - java and html files

---
+++ TCCHANGEFORGOTPASSWORD.java
@@ -0,0 +1,104 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TCCHANGEFORGOTPASSWORD {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://county-bedtracking-system.herokuapp.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTCCHANGEFORGOTPASSWORD() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.linkText("Login")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("nsundaragopalan@unomaha.edu");
+    driver.findElement(By.linkText("Forgot Your Password?")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("nsundaragopalan@unomaha.edu");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.id("security_answer1")).clear();
+    driver.findElement(By.id("security_answer1")).sendKeys("mit");
+    driver.findElement(By.id("security_answer2")).clear();
+    driver.findElement(By.id("security_answer2")).sendKeys("rec");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("tcs@123");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.linkText("Home")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("nsundaragopalan@unomaha.edu");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("tcs@123");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.linkText("Narahari")).click();
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ TCLOGINALLUSERS.java
@@ -0,0 +1,101 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TCLOGINALLUSERS {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://county-bedtracking-system.herokuapp.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTCLOGINALLUSERS() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.linkText("Login")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("hbtsadmin@unomaha.edu");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("secret");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.linkText("Administrator")).click();
+    driver.findElement(By.linkText("Logout")).click();
+    driver.findElement(By.linkText("Login")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("nsundaragopalan@unomaha.edu");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("secret");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.linkText("Narahari")).click();
+    driver.findElement(By.linkText("Logout")).click();
+    driver.findElement(By.linkText("Login")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("araghu@unomaha.edu");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("secret");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.linkText("Archana")).click();
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
